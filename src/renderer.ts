9a6a03a12f5e24bf428dcbe08f8efa0584f3a685
Vidul Nikolaev Petrov <vidul.petrov@gmail.com>
2021-08-31 09:00:32 +0300

    fix loader's building process

--- src/renderer.ts
+++ src/renderer.ts
@@ -26,8 +26,7 @@
  * ```
  */
 
-// import './index.css';
-
+import './index.css';
 import './react/index';
 
 // console.log('👋 This message is being logged by "renderer.js", included via webpack');
--- webpack.main.config.js
+++ webpack.main.config.js
@@ -1,14 +1,28 @@
+/* eslint @typescript-eslint/no-var-requires: "off" */
+
+const CopyPlugin = require("copy-webpack-plugin");
+
 module.exports = {
-  /**
-   * This is the main entry point for your application, it's the first file
-   * that runs in the main process.
-   */
-  entry: './src/index.ts',
-  // Put your normal webpack config below here
-  module: {
-    rules: require('./webpack.rules'),
-  },
-  resolve: {
-    extensions: ['.js', '.ts', '.jsx', '.tsx', '.css', '.json']
-  },
+    /**
+     * This is the main entry point for your application, it's the first file
+     * that runs in the main process.
+     */
+    entry: "./src/index.ts",
+    // Put your normal webpack config below here
+    module: {
+        rules: require("./webpack.rules"),
+    },
+    resolve: {
+        extensions: [".js", ".ts", ".jsx", ".tsx", ".css", ".json"],
+    },
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: "src/loading.html",
+                    to: "loading.html",
+                },
+            ],
+        }),
+    ],
 };
